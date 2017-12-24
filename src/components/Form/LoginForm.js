967c669a8fe11e2317eefa3e96208a526e852f2e
vananhnt <vananhnt97@gmail.com>
2017-12-25 00:00:58 +0700

    fix merge

--- src/components/Form/LoginForm.js
+++ src/components/Form/LoginForm.js
@@ -18,14 +18,14 @@ export class LoginForm extends React.Component {
     componentDidMount() {
       fetch('http://localhost:3001/api/v1/login',{
 			method: 'POST',
-			//mode: 'no-cors',
+			mode: 'no-cors',
       body: {
         'username': 'hoanv',
         'password': '12345678' 
       }
 		}).then(result => {return result.json;
 		}).then(data => {
-			console.log('1')
+			console.log(data)
 		});
     }
     handleChange(e) {
